a4784e6605099bbcff875e7da457810e6b6a7802
bynoura <nr.abdullah06@gmail.com>
2026-01-30 14:54:04 +0300

    Update __init__.py

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -28,10 +28,12 @@ def create_app(config_name='development'):
     from app.api.v1.amenities import api as amenities_ns
     from app.api.v1.places import api as places_ns
     from app.api.v1.reviews import api as reviews_ns
+    from app.api.v1.places import api as places_ns
     
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
+    api.add_namespace(places_ns, path='/api/v1')
     
     return app
